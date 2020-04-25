9df7bcb5d6ccee37dd543ad1a1434ff413025b17
infsolution <clsinfsolution@gmail.com>
2020-04-25 16:34:24 -0300

    update database

--- balloon/settings.py
+++ balloon/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'balloon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'prehistory',
+       'NAME': 'balloondb',
        'USER': 'postgres',
        'PASSWORD': 'postgres',
        'HOST': '127.0.0.1',
        'PORT': '5432',
   }
-}'''
+}
 
 
 # Password validation
